60dd63e8d4b795eb750ab6e71e697932ede3e5f5
Kaivan Shah <48644270+kaivanshah14@users.noreply.github.com>
2022-10-13 12:59:11 +0530

    Add files via upload

---
+++ Electricity Bill Application SQL files/DDL.sql
@@ -0,0 +1,21 @@
+use eba;
+
+drop table consumption;
+drop table customer;
+
+CREATE TABLE `customer` (
+  `id` int NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `customertype` varchar(255) DEFAULT NULL,
+  `areaCode` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+create table consumption(
+billid int auto_increment primary key,
+bill_month date NOT NULL,
+units_consumed int NOT NULL DEFAULT 0,
+totalbill decimal(8,2),
+customer_id int NOT NULL, 
+foreign key(customer_id) references customer(id)
+);
